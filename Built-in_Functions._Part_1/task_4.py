2d261ec12548c27f0a66f98c49df0b4ecd0fb514
Лев Романович Панас <jieba2005@list.ru>
2025-05-05 16:32:50 +0300

    Tasks have been added

---
+++ Built-in_Functions._Part_1/task_4.py
@@ -0,0 +1,18 @@
+"""
+Функция non_negative_even()
+Реализуйте функцию non_negative_even(),  которая принимает один аргумент:
+numbers — непустой список чисел
+Функция должна возвращать True, если все числа в списке numbers являются четными и неотрицательными, или False в противном случае.
+"""
+
+
+def non_negative_even():
+    pass
+
+
+print(non_negative_even([0, 2, 4, 8, 16]))
+# True
+print(non_negative_even([-8, -4, -2, 0, 2, 4, 8]))
+# False
+print(non_negative_even([0, 0, 0, 0, 0]))
+# True
---
+++ Built-in_Functions._Part_1/task_6.py
@@ -0,0 +1,23 @@
+"""
+Функция custom_isinstance()
+Реализуйте функцию custom_isinstance(), которая принимает два аргумента в следующем порядке:
+objects — список произвольных объектов
+typeinfo — тип данных или кортеж с типами
+Функция должна возвращать единственное число — количество объектов из списка objects,
+    которые принадлежат типу typeinfo или одному из типов, если был передан кортеж.
+"""
+
+
+def custom_isinstance():
+    pass
+
+
+numbers = [1, "two", 3.0, "четыре", 5, 6.0]
+print(custom_isinstance(numbers, int))
+# 2
+numbers = [1, "two", 3.0, "четыре", 5, 6.0]
+print(custom_isinstance(numbers, (int, float)))
+# 4
+numbers = [1, "two", 3.0, "четыре", 5, 6.0]
+print(custom_isinstance(numbers, list))
+# 0
