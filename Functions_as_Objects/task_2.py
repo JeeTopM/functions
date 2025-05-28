91b1e87513b464482eb6571c8cb80639421ce660
Лев Романович Панас <jieba2005@list.ru>
2025-05-28 17:04:54 +0300

    task_2 done

--- Functions_as_Objects/task_2.py
+++ Functions_as_Objects/task_2.py
@@ -4,10 +4,16 @@
 чтобы она печатала все переданные строковые аргументы в верхнем регистре.
 """
 
-# ?
-def print():
-    pass
+old_print = print
+
+
+def print(*args, sep=" ", end="\n"):
+    caps = (c.upper() if isinstance(c, str) else c for c in args)
+    old_print(*caps, sep=sep.upper(), end=end.upper())
 
 
 print("beegeek", [1, 2, 3], 4)  # BEEGEEK [1, 2, 3] 4
 print("bee", "geek", sep=" and ", end=" wow")  # BEE AND GEEK WOW
+
+words = ("black", "white", "grey", "black-1", "white-1", "python")
+print(*words, sep=" to ", end=" LOVE")
