b1b96b559c6f9d3a0766fa3b6629aa23cb4aeaa0
Лев Романович Панас <jieba2005@list.ru>
2025-05-28 12:56:27 +0300

    add Functions_as_Objects

---
+++ Functions_as_Objects/task_4.py
@@ -0,0 +1,23 @@
+"""
+Функция remove_marks()
+Реализуйте функцию remove_marks(), которая принимает два аргумента в следующем порядке:
+text — произвольная строка
+marks — набор символов
+Функция должна возвращать строку text, предварительно удалив из нее все символы, перечисленные в строке marks.
+Также функция remove_marks() должна иметь атрибут count, представляющий собой количество вызовов данной функции.
+"""
+
+
+def remove_marks(text, marks):
+    pass
+
+
+text = "Hi! Will we go together?"
+
+print(remove_marks(text, "!?"))
+print(remove_marks.count)
+
+"""
+Hi Will we go together
+1
+"""
