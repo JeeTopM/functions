1b6bd20078a900cd42cce42aeea9856da86605ff
Лев Романович Панас <jieba2005@list.ru>
2025-06-04 12:52:22 +0300

    add 4 task

---
+++ TypeAnnotations/task_1.py
@@ -0,0 +1,21 @@
+"""
+Функция get_digits()
+Реализуйте функцию get_digits() c использованием аннотаций типов, которая принимает один аргумент:
+number — положительное целое или вещественное число
+Функция должна возвращать список, состоящий из цифр числа number.
+"""
+
+
+def get_digits(number):
+    pass
+
+
+print(get_digits(16733))
+# [1, 6, 7, 3, 3]
+
+print(get_digits(13.909934))
+# [1, 3, 9, 0, 9, 9, 3, 4]
+
+annotations = get_digits.__annotations__
+print(annotations["return"])
+# list[int]
---
+++ TypeAnnotations/task_2.py
@@ -0,0 +1,22 @@
+"""
+Функция top_grade()
+Реализуйте функцию top_grade() c использованием аннотаций типов, которая принимает один аргумент:
+grades — словарь, содержащий данные об ученике, а именно имя по ключу name и список оценок по ключу grades
+Функция должна возвращать словарь, содержащий имя ученика по ключу name и его самую высокую оценку по ключу top_grade.
+"""
+
+
+def top_grade(grades):
+    pass
+
+
+info = {"name": "Timur", "grades": [30, 57, 99]}
+print(top_grade(info))
+# {'name': 'Timur', 'top_grade': 99}
+
+print(top_grade({"name": "Ruslan", "grades": [19, 48, 86, 45, 32]}))
+# {'name': 'Ruslan', 'top_grade': 86}
+
+annotations = top_grade.__annotations__
+print(annotations["grades"])
+# dict[str, str | list[int]]
