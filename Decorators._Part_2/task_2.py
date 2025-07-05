92ebb2edf63d694524f06f33a3a1c5f2e1eefb66
Лев Романович Панас <jieba2005@list.ru>
2025-07-05 17:05:36 +0300

    add task2

---
+++ Decorators._Part_2/task_2.py
@@ -0,0 +1,29 @@
+"""
+Декоратор returns_string
+Реализуйте декоратор returns_string, который проверяет, что возвращаемое значение декорируемой функции принадлежит типу str.
+Если возвращаемое значение принадлежит какому-либо другому типу, декоратор должен возбуждать исключение TypeError.
+Также декоратор должен сохранять имя и строку документации декорируемой функции.
+"""
+
+
+def returns_string():
+    pass
+
+
+# 1
+@returns_string
+def beegeek():
+    return "beegeek"
+print(beegeek())
+# beegeek
+
+
+# 2
+@returns_string
+def add(a, b):
+    return a + b
+try:
+    print(add(3, 7))
+except TypeError as e:
+    print(type(e))
+# <class 'TypeError'>
